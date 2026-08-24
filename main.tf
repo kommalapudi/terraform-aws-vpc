7c9706ebe7395b60be718bbea5b5b6fe1c9c6e2b
chaitanya <kommalkrishna@gmail.com>
2026-08-24 15:19:30 +0530

    tags format update

--- main.tf
+++ main.tf
@@ -124,7 +124,7 @@ resource "aws_eip" "nat" {
          {
             Name = "${var.project}-${var.environment}-nat"
          },
-         var.eip.tags
+        var.eip_tags
         
     )
 }
@@ -150,13 +150,13 @@ resource "aws_nat_gateway" "main" {
 resource "aws_route" "private" {
   route_table_id            = aws_route_table.private.id
   destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = aws_internet_gateway.main.id
+  nat_gateway_id = aws_nat_gateway.main.id
 }
 
 resource "aws_route" "database" {
   route_table_id            = aws_route_table.database.id
   destination_cidr_block    = "0.0.0.0/0"
-  nat_gateway_id = aws_internet_gateway.main.id
+  nat_gateway_id = aws_nat_gateway.main.id
 }
 
 
